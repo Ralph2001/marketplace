5e747c3bebc0b54bcd8504547daffecdaa3d3023
Ralph2001 <villanuevaralph2001@gmail.com>
2025-07-06 15:10:04 +0800

    refactor: Update message API routes to use new Supabase client and remove unused auth utility

--- src/components/messages/side-bar.tsx
+++ src/components/messages/side-bar.tsx
@@ -1,44 +1,45 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useAuth } from "../../../context/AuthContext";
-import { supabase } from "../../../libs/supabase";
+
 import Link from "next/link";
+import { createClient } from "../../../utils/supabase/client";
 
 type ChatUser = {
   email: string;
 };
 
 export default function Sidebar() {
-  const { user } = useAuth();
   const [chatUsers, setChatUsers] = useState<ChatUser[]>([]);
 
-  useEffect(() => {
-    if (!user) return;
+  const supabase = createClient();
 
-    const fetchChatUsers = async () => {
-      const { data, error } = await supabase.rpc("get_unique_chat_partners", {
-        current_user: user.email,
-      });
+  // useEffect(() => {
+  //   if (!user) return;
 
-      if (!error) {
-        setChatUsers(data);
-      }
-    };
+  //   const fetchChatUsers = async () => {
+  //     const { data, error } = await supabase.rpc("get_unique_chat_partners", {
+  //       current_user: user.email,
+  //     });
 
-    fetchChatUsers();
-  }, [user]);
+  //     if (!error) {
+  //       setChatUsers(data);
+  //     }
+  //   };
+
+  //   fetchChatUsers();
+  // }, [user]);
 
   return (
     <aside className="w-full md:w-64 bg-white border-r p-4 overflow-y-auto">
-      <h2 className="text-lg font-bold mb-4">Messages</h2>
+      {/* <h2 className="text-lg font-bold mb-4">Messages</h2>
       {chatUsers.map(({ email }) => (
         <Link href={`/messages/${encodeURIComponent(email)}`} key={email}>
           <div className="py-2 px-3 hover:bg-gray-100 rounded cursor-pointer">
             {email}
           </div>
         </Link>
-      ))}
+      ))} */}
     </aside>
   );
 }
